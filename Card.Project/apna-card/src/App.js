41881676708c2698d506ff48df87f0634d0c3dad
shaikhanis <anisshaikh19059@gmail.com>
2023-02-11 00:14:25 +0530

    working on card app

---
+++ Card.Project/apna-card/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import navbar from './Component/navbar';
+
+function App() {
+  return (
+    <>
+    
+   <navbar/>
+    
+    </>
+  );
+}
+
+export default App;
