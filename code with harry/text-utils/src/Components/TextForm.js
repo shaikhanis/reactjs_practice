929f74a46d74cdfa40644da09188088110df401d
shaikhanis <anisshaikh19059@gmail.com>
2023-02-18 23:02:21 +0530

    lecture nuber 8 complitared

--- code with harry/text-utils/src/Components/TextForm.js
+++ code with harry/text-utils/src/Components/TextForm.js
@@ -2,16 +2,41 @@ import React, { useState } from 'react';
 
 
 const TextForm = (props) => {
+  const handleOnChange =(e) =>{
+    setText(e.target.value)
+  }
+
+  const handelUpCase = () =>{
+    let newText = text.toUpperCase();
+    setText(newText);
+  }
+
+const handeleLow= () =>{
+  let newText = text.toLocaleLowerCase();
+  setText(newText);
+}
   
-const [text, setText] = useState("Enter Text Here");
+const [text, setText] = useState("");
 
   return (
-    <div>
-    <h3>{props.text}</h3>
+    <>
+    <div className='container my-3 mx-2'>
+    <h1>{props.text}</h1>
     <div className='mb-3'>
-      <textarea className='form-control' name="text" id="myBox" rows="8"></textarea>
+      <textarea className='form-control' name="text" id="myBox" rows="8" value={text} onChange = {handleOnChange}></textarea>
+      <button className="btn btn-info my-3 mx-2" onClick={handelUpCase}>Conver To Upper case</button>
+      <button className="btn btn-info mx-3" onClick={handeleLow}>Conver To Lower case</button>
+
     </div>
   </div>
+  <div className="container my-3 mx-2">
+    <h1>Your Text Summary</h1>
+    <p> Number of World is [{text.split(" ").length-1}] and Number of Charecter is [{text.length}]</p>
+    <p>{0.087 * text.split(" ").length} Minit Read</p>
+    <h2>Preveiw</h2>
+    <p>{text}</p>
+  </div>
+  </>
   )
 }
 
