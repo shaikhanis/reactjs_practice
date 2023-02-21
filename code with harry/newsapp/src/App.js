600995fab47cce9a50b808fa65334d212fed68da
shaikhanis <anisshaikh19059@gmail.com>
2023-02-21 23:17:35 +0530

    start working with newsapp

---
+++ code with harry/newsapp/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+import React, { Component } from 'react'
+
+export default class App extends Component {
+   c= 'anis';
+  render() {
+    return (
+      <div>
+        Hii.. {this.c}
+      </div>
+    )
+  }
+}
+
