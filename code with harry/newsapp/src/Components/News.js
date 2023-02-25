0cfd59655563bb6bda030eff2cc4521004324ec7
shaikhanis <anisshaikh19059@gmail.com>
2023-02-25 22:58:40 +0530

    adding some style

--- code with harry/newsapp/src/Components/News.js
+++ code with harry/newsapp/src/Components/News.js
@@ -12,7 +12,6 @@ export default class News extends Component {
     return (
       <div className="container my-3">
         <h1>NewsMonkt - Top HeadLine</h1>
-        this ids a news Component
         <div className="row">
           <div className="col-md-4">
             <NewsItem title="myTilte" discription="myDiscription" imageUrl="https://img-s-msn-com.akamaized.net/tenant/amp/entityid/AA17TNci.img?w=1920&h=1080&q=60&m=2&f=jpg"/>
--- code with harry/newsapp/src/Components/NewsItem.js
+++ code with harry/newsapp/src/Components/NewsItem.js
@@ -5,16 +5,16 @@ export class NewsItem extends Component {
     let {title, discription, imageUrl} = this.props;
     return (
       <div>
-        <div className="card mb-3 text-light" style={{ width: "26rem"}}>
-          <img
+        <div className="card mb-3 text-light cards" style={{ width: "26rem"}}>
+          <img 
             src={imageUrl}
             className="card-img"
             alt="Imahges"
           />
           <div className="card-img-overlay">
-            <h3 className="card-title">{title}</h3>
-            <strong className="card-text">{discription}</strong><br/>
-            <a to="/gotosome" class="btn btn-sm btn-info my-2 mx-3 position-absolute bottom-0 end-0"> Read More </a>
+            <h3 className="card-title title">{title}...</h3>
+            <strong className="card-text heading">{discription}...</strong><br/>
+            <a to="/gotosome" class="btn btn-sm my-2 mx-3 position-absolute bottom-0 end-0 button"> Read More </a>
           </div>
         </div>
       </div>
