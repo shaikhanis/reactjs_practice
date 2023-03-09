9e25c85d782a667bf3a9c85deee72e764c6c35af
shaikhanis <anisshaikh19059@gmail.com>
2023-03-09 15:30:26 +0530

    working on some Styling part

--- code with harry/newsapp/src/Components/NewsItem.js
+++ code with harry/newsapp/src/Components/NewsItem.js
@@ -16,11 +16,11 @@ export class NewsItem extends Component {
 
                         <span className="badge rounded-pill bg-danger"> {source} </span>
                     </div>
-                    <img src={!imageUrl ?"https://th.bing.com/th/id/OIP.0Tv0eZ2ayv6GEZMeyuF7cgHaDt?pid=ImgDet&rs=1": imageUrl} className="card-img-top" alt="..." />
-                    <div className="card-body">
-                        <h5 className="card-title">{title}  </h5>
-                        <p className="card-text">{description}</p>
-                        <p className="card-text"><small className="text-muted">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
+                    <img src={!imageUrl ?"https://th.bing.com/th/id/OIP.0Tv0eZ2ayv6GEZMeyuF7cgHaDt?pid=ImgDet&rs=1": imageUrl} className="card-img" alt="Image" />
+                    <div className="card-img-overlay">
+                        <h5 className="card-title title">{title}  </h5>
+                        <p className="card-text heading">{description}</p>
+                        <p className="card-text my-3"><small className="text-light my-3">By {!author ? "Unknown" : author} on  {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn-outline-info btn-sm my-1 mx-2 position-absolute bottom-0 end-0 button">Read More</a>
                     </div>
                 </div>
